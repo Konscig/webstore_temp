999ee30b1e65a2f623b466d97a51dd13d4a2dd02
Герасимов Константин <work.konstantin.gs@gmail.com>
2025-05-30 14:25:37 +0500

    init flutter project

---
+++ tralalero_tralala/README.md
@@ -0,0 +1,3 @@
+# tralalero_tralala
+
+A new Flutter project.
---
+++ tralalero_tralala/android/app/src/main/kotlin/com/example/tralalero_tralala/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tralalero_tralala
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
